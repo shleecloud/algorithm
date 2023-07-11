64ab9dabf65bcb87e9302d9fc51ea0adee927039
shleecloud <shleecloud@gmail.com>
2023-07-11 18:18:15 +0900

    이진탐색 기초

--- Reference/checkPrimeNumber.js
+++ Reference/checkPrimeNumber.js
@@ -1,7 +1,11 @@
-const isPrimeNumber = (n) => {
-    if (n < 2) return false;
+const checkPrimeNumber = (n) => {
+    if (n < 2) {
+        return false;
+    }
+
     for (let i = 2; i <= Math.sqrt(n); i++) {
         if (n % i === 0) return false;
     }
+
     return true;
 };
