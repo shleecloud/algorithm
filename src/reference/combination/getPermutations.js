a5eac854e91acde51ce0fc603a986024eff0290d
shleecloud <shleecloud@gmail.com>
2023-07-28 18:34:47 +0900

    change dir

---
+++ src/reference/combination/getPermutations.js
@@ -0,0 +1,13 @@
+const getPermutations = (arr, selectNumber) => {
+  const results = [];
+  if (selectNumber === 1) return arr.map((value) => [value]);
+
+  arr.forEach((fixed, index, origin) => {
+    const rest = [...origin.slice(0, index), ...origin.slice(index + 1)];
+    const permutations = getPermutations(rest, selectNumber - 1);
+    const attached = permutations.map((el) => [fixed, ...el]);
+    results.push(...attached);
+  });
+
+  return results;
+};
