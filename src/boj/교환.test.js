d784b0e4ea98f298d23bd39c3336f074ef70bf74
shleecloud <shleecloud@gmail.com>
2023-09-21 17:18:19 +0900

    solve 교환

--- src/boj/교환.test.js
+++ src/boj/교환.test.js
@@ -1,4 +1,8 @@
-const solution = (input) => {
+/** https://www.acmicpc.net/problem/1039
+ * BFS, 또는 DFS 사용해야 함
+ * 첫 시도에선 변환한 시점에서 최적의 숫자로 이동을 했는데 이럴 경우 중간 값에 의하여 최고 효율이 나올 수 없음
+ * ex) 6399 일 경우를 해결 할 수 없다 */
+const failedSolution = (input) => {
     debugger;
     let [numList, count] = input.split(' ');
 
@@ -35,13 +39,55 @@ const solution = (input) => {
     return largestNum;
 };
 
-// const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-// const result = solution(input);
-// if (Array.isArray(result)) {
-//     console.log(result.join('\n'));
-// } else {
-//     console.log(result);
-// }
+const solution = (input) => {
+    const inputArr = input[0].split(' ');
+    const numArr = inputArr[0].split('');
+    const maxCount = +inputArr[1];
+
+    let max = -1;
+    const dp = new Array(maxCount + 1) //
+        .fill(null)
+        .map((_) => new Array(1000001).fill(false));
+
+    const swap = (i, j) => {
+        [numArr[i], numArr[j]] = [numArr[j], numArr[i]];
+    };
+
+    const dfs = (count = 0) => {
+        const sum = Number(numArr.join(''));
+        if (dp[count][sum] === true) return;
+        if (count === maxCount) {
+            if (sum > max) {
+                max = sum;
+            }
+            return;
+        }
+
+        for (let i = 0; i < numArr.length; i++) {
+            for (let j = i + 1; j < numArr.length; j++) {
+                swap(i, j);
+                if (numArr[0] !== '0') {
+                    dfs(count + 1);
+                }
+                swap(i, j);
+            }
+        }
+        dp[count][sum] = true;
+    };
+
+    dfs();
+    return String(max);
+};
+
+const input = [];
+require('readline')
+    .createInterface({input: process.stdin})
+    .on('line', (line) => input.push(line.trim()))
+    .on('close', (_) => {
+        const result = solution(input);
+        console.log(Array.isArray(result) ? result.join('\n') : result);
+        process.exit(0);
+    });
 
 test('교환', () => {
     expect(solution('16375 1')).toEqual('76315');
